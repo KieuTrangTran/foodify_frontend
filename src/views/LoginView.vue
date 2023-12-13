08708b854bc19d4232d9cc1aadcd4986ccbd8f69
Jordan Michel Schultka <jordanschultka@gmail.com>
2023-12-13 01:42:23 +0100

    Bugfixes on Sign In and Register Page, added Icons

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -15,6 +15,7 @@
                     <fieldset class="container">
                         <label for="email" class="email-label"><b>Login </b><input id="email" name="email" type="email"
                                                                placeholder="Enter Email" required/></label>
+
                         <label for="new-password" class="new-password-label"><b>Password </b><input id="password" name="password" type="password"
                                                                          pattern="[a-z0-5]{8,}"
                                                                          placeholder="Enter Password" required/></label>
@@ -22,8 +23,12 @@
 
                         <div class="extra-container">
                             <div class="remember-container">
-                                <input type="checkbox" checked="checked" name="remember"> Remember me
-                            </div>
+    <label class="switch">
+        <input type="checkbox" v-model="rememberMe">
+        <span class="slider round"></span>
+    </label>
+    Remember me
+</div>
 
                             <div class="password">
                                 <a href="#">Forgot password?</a>
@@ -40,15 +45,18 @@
             </div>
             <div class="google-apple">
                 <button class="google">Continue with Google</button>
-                <br>
                 <button class="apple">Continue with Apple</button>
                 <div class="NoAccount">
                 <p>Don't have an account?</p>
-                    <a href="#" >Sign up now</a>
+                    <a href="/register" >Sign up now</a>
                 </div>
             </div>
             </div>
         </div>
+        <div class="backgroundicons-login">
+        <img src="src/assets/BakedIcon.svg" alt="Baked Icon" class="baked-icon-login"/>
+        <img src="src/assets/BurgerIcon.svg" alt="Burger Icon" class="burger-icon-login"/>
+    </div>
     </main>
     <FooterComponent/>
 </template>
@@ -60,17 +68,121 @@ import FooterComponent from "@/components/FooterComponent.vue";
 
 export default {
     name: "LoginView",
-    components: {FooterComponent, NavBar}
+    components: {FooterComponent, NavBar},
+    data() {
+        return {
+            rememberMe: true
+        };
+    }
 }
 </script>
 
 <style>
-.hauptsite2 {
-    margin-top: 64px; /*Damit der Banner unter der Navbar ist*/
-    height: 750px;
+
+.remember-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.backgroundicons-login {
+    position: absolute;
+    z-index: 1;
+    overflow: hidden;
+    max-width: 100%;
+    top: 350px;
+}
+
+.baked-icon-login, .burger-icon-login {
+    z-index: 1;
+    overflow: hidden;
+}
+
+.baked-icon-login {
+    rotate: -120deg;
+    height: auto;
+    width: 557px;
+    margin-left: -15vw;
+    margin-top: 250px;
+}
+
+.burger-icon-login {
+    rotate: -17deg;
+    height: auto;
+    width: 444px;
+    margin-left: 70vw;
+    margin-top: -430px;
+}
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 40px;
+  height: 23px;
+  margin-right: 10px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(16px);
+  -ms-transform: translateX(16px);
+  transform: translateX(16px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
 }
 
 
+.hauptsite2 {
+    margin-top: 64px; /*Damit der Banner unter der Navbar ist*/
+    height: 850px;
+}
 
 .redbar {
     background-color: #EC4040;
@@ -95,15 +207,22 @@ export default {
 }
 
 .content-container-account {
+    z-index: 2;
+    position: relative;
 }
 
-
+#password {
+    background-image: url('src/assets/EyeIcon.svg'); /* path to your custom arrow icon */
+    background-repeat: no-repeat;
+    background-position: right 20px center; /* adjust as necessary */
+    background-size: 18px;
+}
 
 .sign-in-section h2 {
     font-size: 25px;
     text-align: center;
     font-weight: bold;
-    padding-top: 40px;
+    padding-top: 100px;
     padding-bottom: 10px;
 }
 
@@ -131,14 +250,16 @@ input[type=email], input[type=password] {
 .sign-in {
     background-color: #EC4040;
     color: white;
-    margin-top: 25px;
+    margin-top: 40px;
     margin-left: 0 !important;
     margin-right: 0 !important;
     border: none;
     cursor: pointer;
-    height: 48px;
+    height: 54px;
     border-radius: 10px;
     box-shadow: 2px 6px 4px rgba(0, 0, 0, 0.15);
+    font-size: 20px;
+    font-weight: bold;
 
 }
 
@@ -155,6 +276,7 @@ input[type=email], input[type=password] {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+    margin-top: 8px;
 }
 
 .line-login {
@@ -177,23 +299,27 @@ input[type=email], input[type=password] {
 .google,
 .apple {
     width: 100%;
-    height: 48px;
+    height: 54px;
     padding: 12px 14px;
-    margin: 8px 0;
-    border: 1px solid #e5e5e5;
-    border-radius: 10px;
-    box-shadow: 2px 6px 4px rgba(0, 0, 0, 0.15);
+    margin: 10px 0;
+    border-radius: 10%;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+    font-size: 20px;
 }
 
 .google {
     color: #808080;
     background-color: white;
     cursor: pointer;
+
+    background-image: url('src/assets/GoogleLogo.png'); /* path to your custom arrow icon */
+    background-repeat: no-repeat;
+    background-position: left 160px center; /* adjust as necessary */
+    background-size: 24px;
 }
 
 .google:hover {
-    background-color: #e5e5e5;
-    color: #808080;
+    background-color: #eeeeee;
     transition: 0.3s;
 
 }
@@ -204,12 +330,15 @@ input[type=email], input[type=password] {
     border: none;
     cursor: pointer;
 
+    background-image: url('src/assets/AppleIcon.png'); /* path to your custom arrow icon */
+    background-repeat: no-repeat;
+    background-position: left 160px center; /* adjust as necessary */
+    background-size: 24px;
+
 }
 
 .apple:hover {
-    background-color: #e5e5e5;
-    color: #808080;
-    border: 1px solid #e5e5e5;
+    background-color: #2f2f2f;
     transition: 0.3s;
 
 
@@ -222,11 +351,13 @@ input[type=email], input[type=password] {
     flex-direction: row;
     width: 100%;
     margin-top: 10px;
+    font-size: 14px;
 }
 
 .NoAccount a {
     text-decoration: none;
 }
+
 
 @media (max-width: 570px) {
     .NoAccount {
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,26 +1,26 @@
 <template>
     <NavBar/>
-    <main class="hauptsite2">
+    <main class="hauptsite3">
         <div class="redbar">
             <h1 class="redbar-text">ACCOUNT</h1>
         </div>
         <div class="box">
-            <div class="content-container-account">
+            <div class="content-container-account-reg">
 
-                <div class="sign-in-section">
+                <div class="sign-up-section">
 
                     <h2>SIGN UP</h2>
 
                     <form method="post" action='#'>
                         <fieldset class="container">
-                            <label for="email" class="email-label"><b>Login </b><input id="email" name="email" type="email"
+                            <label for="email" class="email-label-reg"><b>Login </b><input id="email" name="email" type="email"
                                                                                        placeholder="Enter Email" required/></label>
-                            <label for="new-password" class="new-password-label"><b>Password </b><input id="password" name="password" type="password"
+                            <label for="new-password" class="new-password-label-reg"><b>Password </b><input id="password-reg" name="password" type="password"
                                                                                                         pattern="[a-z0-5]{8,}"
                                                                                                         placeholder="Enter Password" required/></label>
-                            <label for="repeat-password" class="repeat-password-label"><b>Repeat Password </b><input id="password" name="repeat-password" type="password"
+                            <label for="repeat-password" class="repeat-password-label-reg"><b>Repeat Password </b><input id="repeat-password-reg" name="repeat-password" type="password"
                                                                                                         pattern="[a-z0-5]{8,}"
-                                                                                                        placeholder="Enter Password" required/></label>
+                                                                                                        placeholder="Repeat Password" required/></label>
 
 
                             <div class="terms-container">
@@ -39,13 +39,19 @@
                 <div class="google-apple">
                     <button class="google">Sign Up with Google</button>
                     <button class="apple">Sign Up with Apple</button>
-                    <div class="NoAccount">
-                        <p>Don't have an account?</p>
-                        <a href="#">Sign up now</a>
+                    <div class="AlrdyAccount">
+                        <p>Already have an account?</p>
+                        <a href="/login">Sign in now</a>
                     </div>
                 </div>
             </div>
         </div>
+        
+        <div class="backgroundicons-register">
+        <img src="src/assets/BakedIcon.svg" alt="Baked Icon" class="baked-icon-register"/>
+        <img src="src/assets/BurgerIcon.svg" alt="Burger Icon" class="burger-icon-register"/>
+    </div>
+
     </main>
     <FooterComponent/>
 
@@ -62,9 +68,46 @@ export default {
 </script>
 
 <style>
-.hauptsite2 {
+
+.backgroundicons-register {
+    position: absolute;
+    z-index: 1;
+    overflow: hidden;
+    max-width: 100%;
+    top: 350px;
+}
+
+.baked-icon-register, .burger-icon-register {
+    z-index: 1;
+    overflow: hidden;
+}
+
+.baked-icon-register {
+    rotate: -120deg;
+    height: auto;
+    width: 557px;
+    margin-left: -15vw;
+    margin-top: 250px;
+}
+
+.burger-icon-register {
+    rotate: -17deg;
+    height: auto;
+    width: 444px;
+    margin-left: 70vw;
+    margin-top: -430px;
+}
+
+#repeat-password-reg, #password-reg {
+    background-image: url('src/assets/EyeIcon.svg'); /* path to your custom arrow icon */
+    background-repeat: no-repeat;
+    background-position: right 20px center; /* adjust as necessary */
+    background-size: 18px;
+}
+
+.hauptsite3 {
     margin-top: 64px; /*Damit der Banner unter der Navbar ist*/
-    height: 750px;
+    height: 920px;
 }
 
 
@@ -91,22 +134,24 @@ export default {
     padding: 0;
 }
 
-.content-container-account {
+.content-container-account-reg {
+    z-index: 2;
+    position: relative;
 }
 
 
 
-.sign-in-section h2 {
+.sign-up-section h2 {
     font-size: 25px;
     text-align: center;
     font-weight: bold;
-    padding-top: 40px;
+    padding-top: 100px;
     padding-bottom: 10px;
 }
 
-.email-label b,
-.new-password-label b,
-.repeat-password-label b {
+.email-label-reg b,
+.new-password-label-reg b,
+.repeat-password-label-reg b {
     font-size: 10px;
     font-weight: normal;
     color: #333333;
@@ -127,6 +172,10 @@ input[type=password] {
     background-color: #f2f2f2;
 }
 
+.terms-container {
+    margin-top: 12px;
+}
+
 .terms-container p {
     font-size: 11px;
     text-align: center;
@@ -140,7 +189,9 @@ input[type=password] {
     margin-right: 0 !important;
     border: none;
     cursor: pointer;
-    height: 48px;
+    height: 54px;
+    font-size: 20px;
+    font-weight: bold;
     border-radius: 10px;
     box-shadow: 2px 6px 4px rgba(0, 0, 0, 0.15);
 
@@ -181,23 +232,27 @@ input[type=password] {
 .google,
 .apple {
     width: 100%;
-    height: 48px;
+    height: 54px;
     padding: 12px 14px;
-    margin: 8px 0;
-    border: 1px solid #e5e5e5;
-    border-radius: 10px;
-    box-shadow: 2px 6px 4px rgba(0, 0, 0, 0.15);
+    margin: 10px 0;
+    border-radius: 10%;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+    font-size: 20px;
 }
 
 .google {
     color: #808080;
     background-color: white;
     cursor: pointer;
+
+    background-image: url('src/assets/GoogleLogo.png'); /* path to your custom arrow icon */
+    background-repeat: no-repeat;
+    background-position: left 160px center; /* adjust as necessary */
+    background-size: 24px;
 }
 
 .google:hover {
-    background-color: #e5e5e5;
-    color: #808080;
+    background-color: #eeeeee;
     transition: 0.3s;
 
 }
@@ -208,32 +263,36 @@ input[type=password] {
     border: none;
     cursor: pointer;
 
+    background-image: url('src/assets/AppleIcon.png'); /* path to your custom arrow icon */
+    background-repeat: no-repeat;
+    background-position: left 160px center; /* adjust as necessary */
+    background-size: 24px;
+
 }
 
 .apple:hover {
-    background-color: #e5e5e5;
-    color: #808080;
-    border: 1px solid #e5e5e5;
+    background-color: #2f2f2f;
     transition: 0.3s;
 
 
 }
 
-.NoAccount {
+.AlrdyAccount {
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: row;
     width: 100%;
     margin-top: 10px;
+    font-size: 14px;
 }
 
-.NoAccount a {
+.AlrdyAccount a {
     text-decoration: none;
 }
 
 @media (max-width: 570px) {
-    .NoAccount {
+    .AlrdyAccount {
         justify-content: space-between;
     }
 
